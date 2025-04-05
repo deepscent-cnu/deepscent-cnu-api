3ef5338f418e0c95e8d6afcc50b800c6c3d990b0
Minji Kim <101392857+alsswl@users.noreply.github.com>
2025-04-05 20:57:35 +0900

    Initial commit

---
+++ src/main/java/com/example/deepsent/DeepsentApplication.java
@@ -0,0 +1,13 @@
+package com.example.deepsent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeepsentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeepsentApplication.class, args);
+	}
+
+}
