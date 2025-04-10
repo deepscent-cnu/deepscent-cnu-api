54628f1eb4001513e60b74b0b9d276433511bdc2
cussle <cussle@kakao.com>
2025-04-10 23:50:42 +0900

    feat: 회원 엔티티 구현

---
+++ src/main/java/deepscent_cnu/deepscent_cnu_api/auth/entity/Member.java
@@ -0,0 +1,35 @@
+package deepscent_cnu.deepscent_cnu_api.auth.entity;
+
+
+import jakarta.persistence.*;
+import lombok.Getter;
+
+@Entity
+@Getter  // 전체 필드의 getter 생성
+public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String birthDate;
+    private String phoneNumber;
+
+    @Column(unique = true)
+    private String username;
+    private String password; // 해시된 비밀번호
+
+    public Member() {
+
+    }
+
+    public Member(Long id, String name, String birthDate, String phoneNumber, String username, String password) {
+        this.id = id;
+        this.name = name;
+        this.birthDate = birthDate;
+        this.phoneNumber = phoneNumber;
+        this.username = username;
+        this.password = password;
+    }
+
+}
