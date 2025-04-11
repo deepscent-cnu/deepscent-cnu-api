183df2a6c81c76473dd1db8b358607e6bf905a04
cussle <cussle@kakao.com>
2025-04-11 14:46:55 +0900

    feat: 회원가입 기능 구현

---
+++ src/main/java/deepscent_cnu/deepscent_cnu_api/auth/controller/AuthController.java
@@ -0,0 +1,26 @@
+package deepscent_cnu.deepscent_cnu_api.auth.controller;
+
+import deepscent_cnu.deepscent_cnu_api.auth.dto.MemberRequest;
+import deepscent_cnu.deepscent_cnu_api.auth.dto.MemberResponse;
+import deepscent_cnu.deepscent_cnu_api.auth.service.MemberService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+    private final MemberService memberService;
+
+    public AuthController(MemberService memberService) {
+        this.memberService = memberService;
+    }
+
+    @PostMapping("/signup")
+    public ResponseEntity<String> signup(@RequestBody MemberRequest request) {
+        MemberResponse memberResponse = memberService.signup(request);
+        return ResponseEntity.ok(memberResponse.token()); // 프론트에서 이 토큰 저장해서 사용
+    }
+}
