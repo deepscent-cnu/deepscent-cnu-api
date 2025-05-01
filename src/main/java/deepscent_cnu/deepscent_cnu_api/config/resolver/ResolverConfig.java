990463e2667f0323026c57049f8fa17e461736fc
cussle <cussle@kakao.com>
2025-05-01 18:57:31 +0900

    refactor: Auth 관련 Interceptor 및 resolver config 등록

--- src/main/java/deepscent_cnu/deepscent_cnu_api/config/resolver/ResolverConfig.java
+++ src/main/java/deepscent_cnu/deepscent_cnu_api/config/resolver/ResolverConfig.java
@@ -1,5 +1,6 @@
 package deepscent_cnu.deepscent_cnu_api.config.resolver;
 
+import deepscent_cnu.deepscent_cnu_api.auth.service.MemberService;
 import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,13 +10,27 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class ResolverConfig implements WebMvcConfigurer {
 
+  private final MemberService memberService;
+
+  public ResolverConfig(MemberService memberService) {
+    this.memberService = memberService;
+  }
+
   @Bean
   public DeepscentTokenMethodArgumentResolver deepscentTokenMethodArgumentResolver() {
     return new DeepscentTokenMethodArgumentResolver();
   }
 
+  @Bean
+  public AuthTokenMethodArgumentResolver authTokenMethodArgumentResolver() {
+    return new AuthTokenMethodArgumentResolver(memberService);
+  }
+
   @Override
   public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+    // DeepScent 토큰 리졸버
     resolvers.add(deepscentTokenMethodArgumentResolver());
+    // 회원 인증용 AuthToken 리졸버
+    resolvers.add(authTokenMethodArgumentResolver());
   }
 }
