4b4d82f47cbc717f64cff8b6b7a5d379b89c47d8
배민수 <duecost@gmail.com>
2025-07-14 22:07:23 +0900

    feat: 서버 실행 시 관리자 계정 자동 생성 로직 추가

--- src/main/java/deepscent_cnu/deepscent_cnu_api/config/interceptor/InterceptorConfig.java
+++ src/main/java/deepscent_cnu/deepscent_cnu_api/config/interceptor/InterceptorConfig.java
@@ -16,23 +16,18 @@ public class InterceptorConfig implements WebMvcConfigurer {
   }
 
   @Bean
-  public DeepscentTokenInterceptor deepscentTokenInterceptor() {
-    return new DeepscentTokenInterceptor();
-  }
-
-  @Bean
   public AuthTokenInterceptor authTokenInterceptor() {
     return new AuthTokenInterceptor(jwtTokenProvider);
   }
 
   @Override
   public void addInterceptors(InterceptorRegistry registry) {
-//    registry.addInterceptor(authTokenInterceptor())
-//        .addPathPatterns("/api/**")            // 모든 API path 에 적용
-//        .excludePathPatterns("/api/auth/signup", "/api/auth/login");  // 회원가입 및 로그인 엔드포인트 제외
     registry.addInterceptor(authTokenInterceptor())
-        .addPathPatterns("/api/auth/withdraw")
-        .addPathPatterns("/api/normal-olfactory-training/log");
+        .addPathPatterns("/api/**")            // 모든 API path 에 적용
+        .excludePathPatterns("/api/auth/signup", "/api/auth/login");  // 회원가입 및 로그인 엔드포인트 제외
+//    registry.addInterceptor(authTokenInterceptor())
+//        .addPathPatterns("/api/auth/withdraw")
+//        .addPathPatterns("/api/normal-olfactory-training/log");
 
   }
 }
